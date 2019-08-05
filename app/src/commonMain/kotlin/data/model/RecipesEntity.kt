42c2c801001b0c2fb3837be8a022432e0ce9789d
Antonio Acien <antonio.acien.gomez@accenture.com>
2019-08-05 11:53:32 +0200

    Add Kotlinx.serialization library and JSON parsing

--- app/src/commonMain/kotlin/data/model/RecipesEntity.kt
+++ app/src/commonMain/kotlin/data/model/RecipesEntity.kt
@@ -1,11 +1,15 @@
 package data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class RecipesEntity(
     val href: String,
     val results: List<Result>,
     val title: String,
     val version: Double
 ) {
+    @Serializable
     data class Result(
         val href: String,
         val ingredients: String,
